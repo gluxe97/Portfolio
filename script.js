f8ee5ff70ea7ca9cde804eb280413e355bd402e1
gluxe97 <96441367+gluxe97@users.noreply.github.com>
2022-01-24 06:06:22 -0600

    Main header, avi, name, and bio added

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+h2.textContent = `BIO: College dropout. Reserve USMC Veteran. Master of none. Student of Life. 
+All terms that can be used to categorize me and place me into a box. Although none 
+truly capture the  experiences I've been through, my personality, or my individuality.
+ I am an ambitious 24 year old with too many hobbies and not enough time in the day. 
+ My main hobbies include Working Out, 3D Modeling, Woodworking, Photography, Fashion,
+and creation in general. I find creating things from nothing but a thought very intriging 
+and to be one of the greatest feelings of achievement you can experience. My curiousity 
+has given me an abudance of knowledge in many fields which allows me to find common interests
+with most people I meet. I am well rounded and pick up new skills very fast. 
+I love sports! My favorite teams include the Dallas Cowboys, Mavericks, and Chicago Bulls.
+`;
+document.querySelector("div.bio").appendChild(h2);
+h2.setAttribute('id', 'bio');
+
